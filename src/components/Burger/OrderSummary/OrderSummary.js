0fef9f49d29ee89476a45672f684379c3b479bf5
cherman <santi.scheurmann@gmail.com>
2019-09-04 16:42:29 -0300

    Changing the structure floders

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Auxilliary from '../../../hoc/Auxilliary';
+import Auxilliary from '../../../hoc/Auxilliary/Auxilliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
