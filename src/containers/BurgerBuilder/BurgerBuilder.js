211fc2cc2f57ac35ba2506a178a9b8de79f6616c
cherman <santi.scheurmann@gmail.com>
2019-10-02 03:19:59 -0300

    Set reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        axios.get('https://burger-builder-61139.firebaseio.com/ingredients.json')
+        /*axios.get('https://burger-builder-61139.firebaseio.com/ingredients.json')
             .then(response =>{
                 this.setState({ingredients: response.data});
             })
             .catch(error => {
                 this.setState({error: true});
-            });
+            });*/
     }
 
     updatePurchaseState (ingredients) {
